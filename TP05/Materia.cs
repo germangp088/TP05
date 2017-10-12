9e6032d974887aeb18870b1f4fda04cdde67bbfd
germangp088 <germangp088@gmail.com>
2017-10-11 23:13:29 -0300

    Subo proyecto.

---
+++ TP05/Materia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05
+{
+    class Materia
+    {
+        public string Nombre { get; set; }
+        public int Anio { get; set; }
+        public int Cuatrimestre { get; set; }
+        public List<Nota> Notas { get; set; }
+    }
+}
---
+++ TP05/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Materia> materias = new List<Materia>();
+            Materia materia1 = new Materia();
+            materia1.Anio = 2016;
+            materia1.Cuatrimestre = 1;
+            materia1.Nombre = "Matematica";
+            materia1.Notas = new List<Nota>();
+
+            Nota nota1m1 = new Nota();
+            nota1m1.Fecha = DateTime.Now;
+            nota1m1.Tipo = "TP";
+            nota1m1.Valor = 9;
+            materia1.Notas.Add(nota1m1);
+
+            Nota nota2m1 = new Nota();
+            nota2m1.Fecha = DateTime.Now;
+            nota2m1.Tipo = "P1";
+            nota2m1.Valor = 9;
+            materia1.Notas.Add(nota2m1);
+
+            Materia materia2 = new Materia();
+            materia2.Anio = 2017;
+            materia2.Cuatrimestre = 2;
+            materia2.Nombre = "POO";
+            materia2.Notas = new List<Nota>();
+
+            Nota nota1m2 = new Nota();
+            nota1m2.Fecha = DateTime.Now;
+            nota1m2.Tipo = "TP";
+            nota1m2.Valor = 7;
+            materia2.Notas.Add(nota1m2);
+
+            Nota nota2m2 = new Nota();
+            nota2m2.Fecha = DateTime.Now;
+            nota2m2.Tipo = "P1";
+            nota2m2.Valor = 8;
+            materia2.Notas.Add(nota2m2);
+            materias.Add(materia1);
+
+
+            //Armar una consulta para obtener todas las materias que cursaron en el primer cuatrimestre:
+            List<Materia> filtro = materias.FindAll(x => x.Cuatrimestre == 1);
+
+            //Aarmar una consulta donde el resultado este ordenado alfabeticamente por nombre:
+            IEnumerable<Materia> filtro2 = materias.FindAll(x => x.Cuatrimestre == 1).OrderBy(y => y.Nombre);
+
+            //Armar una consulta donde se devuelva una lista de todas las notas del tipo TP:
+
+        }
+    }
+}
